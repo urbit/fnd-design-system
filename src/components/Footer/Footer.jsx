c2e1f50dd3599eda83f0b65c6693f824a1afd244
ripperi <22086553+ripperi@users.noreply.github.com>
2023-09-07 14:40:14 +0300

    feat: add Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Footer({ title = "", data = [] }) {
+  return (
+    <footer className="bg-tint text-lite text-xl font-normal mt-20 w-full flex justify-center z-10">
+      <div className="layout">
+        <section className="flex flex-row flex-wrap w-full pt-16 pb-8 ">
+          {data?.[0]?.map((section, i) => {
+            return (
+              <React.Fragment key={i}>
+                {i === 0 && (
+                  <div className="w-full relative leading-none">
+                    <span className="absolute -left-8 leading-4">~</span>
+                    {section.title}
+                  </div>
+                )}
+                <div className="pt-8 w-1/2 md:w-1/3 flex flex-col shrink">
+                  {section.links.map((link) => {
+                    return (
+                      <Link
+                        className="pb-1"
+                        key={link.href}
+                        href={link.href}
+                        passHref
+                      >
+                        {link.title}
+                      </Link>
+                    );
+                  })}
+                </div>
+              </React.Fragment>
+            );
+          })}
+        </section>
+        <section className="flex flex-col md:flex-row w-full pt-16 pb-8 ">
+          {data?.[1]?.map((link) => {
+            return (
+              <div key={link.href} className="md:w-1/3">
+                <Link href={link.href} passHref>
+                  {link.title}
+                </Link>
+              </div>
+            );
+          })}
+        </section>
+      </div>
+    </footer>
+  );
+}
