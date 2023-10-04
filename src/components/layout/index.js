0e2e8f9e1e6fb7f81ef632198766108b4499ec0a
ripperi <22086553+ripperi@users.noreply.github.com>
2023-10-04 18:48:40 +0300

    feat: add MenuTray component

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -3,3 +3,4 @@ export { default as IntraNav } from "./IntraNav";
 export { default as Footer } from "./Footer";
 export { default as Main } from "./Main";
 export { default as SideBar } from "./SideBar";
+export { default as MenuTray } from "./MenuTray";
