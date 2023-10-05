bbd85ed8601fb496126415f335be70b17ff2c00e
ripperi <22086553+ripperi@users.noreply.github.com>
2023-10-05 19:28:32 +0300

    feat: add flag to main for including site paddings

--- src/components/layout/Main/Main.jsx
+++ src/components/layout/Main/Main.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import classnames from "classnames";
 
-export default function Main({ children }) {
+export default function Main({ children, singleColumn = false }) {
   return (
-    <main className="flex flex-col flex-1 layout">
+    <main
+      className={classnames("flex flex-col flex-1 layout", {
+        "layout-px": singleColumn,
+      })}
+    >
       {children}
     </main>
   );
