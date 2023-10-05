c8aba60bdfaeade94656351f548fad1ca1176ccd
ripperi <22086553+ripperi@users.noreply.github.com>
2023-10-05 19:19:03 +0300

    feat: new IntraNav design

--- src/components/layout/IntraNav/IntraNav.jsx
+++ src/components/layout/IntraNav/IntraNav.jsx
@@ -3,9 +3,15 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import classnames from "classnames";
 
-function ActiveLink({ children, href, className, currentPath, dark = false }) {
+function ActiveLink({
+  children,
+  href,
+  target,
+  className,
+  currentPath,
+  dark = false,
+}) {
   const firstCrumb = currentPath.split("/")[1];
-
   const activeClassName = !dark
     ? classnames({
         "text-lite": "/" + firstCrumb === href,
@@ -17,118 +23,126 @@ function ActiveLink({ children, href, className, currentPath, dark = false }) {
       });
 
   return (
-    <Link className={`${className} ${activeClassName}`} href={href} passHref>
+    <Link
+      className={`${className} ${activeClassName}`}
+      href={href}
+      target={target}
+      passHref
+    >
       {children}
     </Link>
   );
 }
 
-export default function IntraNav({ sites, ourSite, pages }) {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const currentPath = useRouter().asPath;
+function Dropdown({ className = "", label, items }) {
+  const [isOpen, setOpen] = useState(false);
 
   return (
+    <div
+      className={`relative flex h-full items-center bg-black w-1/2 md:w-52 lg:w-[14.5rem] xl:w-64 type-ui ${className}`}
+    >
+      {typeof label === "object" && (
+        <Link
+          className="flex flex-1 relative h-full items-center text-gray hover:opacity-80"
+          href="/"
+        >
+          <div className="flex items-center h-full bg-black">
+            <span className="flex justify-center items-center h-full w-5 md:w-8 lg:w-10 xl:w-12 bg-brite">
+              ~
+            </span>
+          </div>
+          <span className="flex items-center h-full w-full bg-brite text-gray">
+            {label.title}
+          </span>
+        </Link>
+      )}
+      <button
+        className={classnames(
+          "flex items-center justify-center h-full hover:opacity-80",
+          {
+            "bg-brite text-gray w-12": typeof label === "object",
+            "bg-gray text-brite w-full px-5": typeof label === "string",
+          }
+        )}
+        onClick={() => setOpen(!isOpen)}
+      >
+        {typeof label === "string" && <span className="mr-auto">{label}</span>}
+        <span>{(isOpen && "↑") || "↓"}</span>
+      </button>
+      {isOpen && (
+        <div className="absolute top-full w-full bg-black">
+          {items.map((item) => {
+            return (
+              <ActiveLink
+                className={classnames(
+                  "flex whitespace-nowrap relative h-16 items-center hover:opacity-80 leading-none",
+                  (typeof label === "object" && "layout-pl") || "pl-5",
+                  item.theme || "",
+                  {
+                    "bg-gray text-brite": !item.theme,
+                    "bg-brite text-gray": item.theme,
+                  }
+                )}
+                href={item.href}
+                target={item.target || "_self"}
+                currentPath={useRouter().asPath}
+              >
+                {item.title}
+              </ActiveLink>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Pages({ className, pages }) {
+  return (
+    <nav
+      className={classnames(
+        "hidden lg:flex items-center nav-space-x-offset",
+        className
+      )}
+    >
+      {pages.map((page) => (
+        <ActiveLink
+          currentPath={useRouter().asPath}
+          className="type-ui"
+          href={page.href}
+          key={page.title}
+        >
+          {page.title}
+        </ActiveLink>
+      ))}
+    </nav>
+  );
+}
+
+export default function IntraNav({ ourSite, sites, pages, search }) {
+  return (
     <div className="sticky top-0 z-50 flex flex-col items-center w-full bg-gray">
-      <div className="relative layout layout-px h-12 md:h-16">
+      <div className="relative layout h-12 md:h-16">
         <div className="flex justify-between items-center h-full">
-          <div className="flex h-full items-center">
-            <div className="relative flex h-full side-bar-with-margin -ml-5 md:-ml-8 lg:-ml-10 xl:-ml-12 items-center bg-black">
-              <a
-                className="flex flex-1 relative h-full items-center text-gray hover:opacity-80"
-                href="/"
-              >
-                <div className="flex items-center h-full bg-black">
-                  <span className="flex justify-center items-center h-full w-5 md:w-8 lg:w-10 xl:w-12 bg-brite">
-                    ~
-                  </span>
-                </div>
-                <span className="flex items-center h-full w-full bg-brite">
-                  {ourSite.title}
-                </span>
-              </a>
-              <button
-                className="h-full w-12 bg-brite text-gray hover:opacity-80 "
-                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-              >
-                {(isDropdownOpen && "↑") || "↓"}
-              </button>
-              {isDropdownOpen && (
-                <div className="hidden md:block absolute top-full w-full bg-black">
-                  {sites.map((site) => (
-                    <a
-                      className={
-                        "flex relative h-16 w-full pl-12 pr-4 items-center " +
-                        "hover:opacity-80 bg-brite text-gray " +
-                        site.theme
-                      }
-                      href={site.href}
-                      key={site.title}
-                    >
-                      {site.title}
-                    </a>
-                  ))}
-                  <a
-                    className="flex whitespace-nowrap relative h-16 pl-12 pr-4 items-center hover:opacity-80 bg-gray text-lite leading-none"
-                    target="_blank"
-                    href="https://network.urbit.org"
-                  >
-                    Network Explorer ↗
-                  </a>
-                </div>
-              )}
-              {isDropdownOpen && (
-                <div
-                  className="absolute block md:hidden z-20 w-screen top-full bg-brite overflow-y-scroll"
-                  style={{height: "calc(100vh - 3rem)"}}
-                >
-                  {sites.map((site) => (
-                    <a
-                      className={
-                        "flex relative h-12 w-full items-center " +
-                        "pl-5 md:pl-8 lg:pl-10 xl:pl-12 " +
-                        "hover:opacity-100 hover:bg-brite text-gray " +
-                        site.theme
-                      }
-                      href={site.href}
-                      key={site.title}
-                    >
-                      {site.title}
-                    </a>
-                  ))}
-                  <a
-                    className="flex whitespace-nowrap relative h-12 pl-5 md:pl-8 lg:pl-10 xl:pl-12 items-center text-gray hover:text-brite hover:bg-gray leading-none"
-                    target="_blank"
-                    href="https://network.urbit.org"
-                  >
-                    Network Explorer ↗
-                  </a>
-                  <div className="h-0.5 w-full rounded-full bg-gray mb-3" />
-                  {pages.map((page) => (
-                    <ActiveLink
-                      currentPath={currentPath}
-                      className="flex relative w-full items-center pl-5 md:pl-8 lg:pl-10 xl:pl-12 py-1"
-                      href={page.href}
-                      key={page.title}
-                      dark
-                    >
-                      {page.title}
-                    </ActiveLink>
-                  ))}
-                </div>
-              )}
+          <div className="flex h-full w-full items-center">
+            <div className="flex flex-1 h-full items-center">
+              <Dropdown label={ourSite} items={sites} />
+              <Dropdown className="lg:hidden" label="Menu" items={pages} />
+              <Pages
+                className="w-1/2 md:flex-1 overflow-x-auto"
+                pages={pages}
+              />
             </div>
-            <nav className="flex items-center hidden md:flex nav-space-x-offset">
-              {pages.map((page) => (
-                <ActiveLink
-                  currentPath={currentPath}
-                  className="type-ui"
-                  href={page.href}
-                  key={page.title}
+            {search && (
+              <div className="h-full p-2 md:p-3 w-1/4 sm:w-1/3 md:w-52 lg:w-[14.5rem] xl:w-64 type-ui">
+                <button
+                  className="h-full w-full bg-brite text-gray rounded-lg"
+                  onClick={search}
                 >
-                  {page.title}
-                </ActiveLink>
-              ))}
-            </nav>
+                  Search
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
