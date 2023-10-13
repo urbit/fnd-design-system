3bb227c1976a94344254bc21d785010412b28ca0
ripperi <22086553+ripperi@users.noreply.github.com>
2023-10-13 18:20:03 +0300

    feat: improve content card responsiveness

--- src/components/ui/ContentCard/ContentCard.jsx
+++ src/components/ui/ContentCard/ContentCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import classnames from "classnames";
+import FatBlock from "../../layout/FatBlock"
 
 export default function ContentCard({
   className,
@@ -11,8 +12,7 @@ export default function ContentCard({
   imgSrc,
   small = false,
 }) {
-  const width = small ? "w-11/12" : "w-11/12 md:w-5/6 xl:w-9/12"
-  const img = small ? "h-44 lg:h-52 pl-44 lg:pl-52" : "h-44 sm:h-52 lg:h-80 pl-44 sm:pl-52 lg:pl-80"
+  const width = small ? "w-11/12" : "w-11/12 md:w-5/6 xl:w-9/12";
   const style = imgSrc
     ? {
         backgroundImage: `url(${imgSrc})`,
@@ -20,14 +20,23 @@ export default function ContentCard({
     : {};
 
   return (
-    <div className={`flex-1 -mx-4 ${className}`}>
-      <div className={`hidden sm:flex p-4 bg-gray rounded-lg ${width}`}>
+    <FatBlock className={className}>
+      <div
+        className={`aspect-[2.5/1] hidden sm:flex p-4 bg-gray rounded-lg ${width}`}
+      >
         <div
-          className={`${img} bg-center bg-cover`}
+          className={`${small ? "aspect-[2/3]" : "aspect-square"} h-full bg-center bg-cover`}
           style={style}
         />
         <div className="flex flex-col items-start justify-between pl-4">
-          <h3 className="h2b md-exclude">{title}</h3>
+          <h3
+            className={classnames(
+              { h3: small, h2: !small },
+              "text-lite md-exclude"
+            )}
+          >
+            {title}
+          </h3>
           <div>
             <p className="body-sm text-lite md-exclude font-semibold mb-5">
               {description}
@@ -45,12 +54,14 @@ export default function ContentCard({
           alt=""
           src={imgSrc}
         />
-        <p className="h2b md-exclude">{title}</p>
-        <p className="body-sm text-lite md-exclude font-semibold">{description}</p>
+        <h3 className="text-lite h2 md-exclude">{title}</h3>
+        <p className="body-sm text-lite md-exclude font-semibold">
+          {description}
+        </p>
         <Link className="btn-sm btn-light md-exclude" href={href}>
           {label}
         </Link>
       </div>
-    </div>
+    </FatBlock>
   );
 }
