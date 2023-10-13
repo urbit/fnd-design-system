ae301ba690ce462c420fca790baf366624f6eca0
ripperi <22086553+ripperi@users.noreply.github.com>
2023-10-13 13:41:23 +0300

    feat: better aspect ratio and responsiveness for post card

--- src/components/ui/PostCard/PostCard.jsx
+++ src/components/ui/PostCard/PostCard.jsx
@@ -24,14 +24,22 @@ export default function PostCard({
 
   return (
     <Link
-      className={`flex flex-col h-[41.5rem] md-exclude ${className}`}
+      className={`flex flex-col aspect-[1.5/2] max-w-xs sm:max-w-md md:max-w-lg md-exclude ${className}`}
       href={href}
     >
       <div
         className="h-2/3 bg-gray bg-center bg-cover rounded-t-lg p-4"
         style={style}
       >
-        <h3 className="h2b md-exclude">{title}</h3>
+        <h3
+          className="h2b md-exclude"
+          style={{
+            "-webkit-text-stroke-width": "1px",
+            "-webkit-text-stroke-color": "var(--gray)",
+          }}
+        >
+          {title}
+        </h3>
       </div>
       <div className="flex flex-col h-1/3 bg-brite text-gray rounded-b-lg p-4">
         <p className="flex-1 body-sm md-exclude font-semibold overflow-hidden mb-auto">
@@ -39,7 +47,7 @@ export default function PostCard({
         </p>
         {(date || authorName || authorPlanet) && (
           <div className="h-1/3">
-            <hr className="hr-horizontal border-gray my-3.5" />
+            <hr className="hr-horizontal border-gray mb-0.5 md:mb-2 xl:mb-3.5" />
             <div className="flex justify-between text-base font-medium">
               <p>{date}</p>
               <div>
