c8a21508d3c8728dc4ac8a9765048b57446587a5
ripperi <22086553+ripperi@users.noreply.github.com>
2023-10-16 16:29:16 +0300

    feat: add VideoCard component

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -6,3 +6,4 @@ export { default as ContentCard } from "./ContentCard";
 export { default as PostCard } from "./PostCard";
 export { default as EventCard } from "./EventCard";
 export { default as LogoCard } from "./LogoCard";
+export { default as VideoCard } from "./VideoCard";
