41dfcf3a29954395295507f3cf877fbe820803a8
ripa <22086553+ripperi@users.noreply.github.com>
2023-12-18 19:49:50 +0200

    fix: conficts with body scroll blocking

--- src/components/markdown/Fence.js
+++ src/components/markdown/Fence.js
@@ -105,10 +105,12 @@ export default function Fence({
   useEffect(() => {
     if (isFullscreen) {
       document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "visible";
     }
-    return () => (document.body.style.overflow = "visible");
+    return () => {
+      if (isFullscreen) {
+        document.body.style.overflow = "visible";
+      }
+    };
   }, [isFullscreen]);
 
   return (
