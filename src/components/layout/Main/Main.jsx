0c6b04d0ba062d7de023f1734fe0c8404e23e409
ripa <22086553+ripperi@users.noreply.github.com>
2024-01-02 17:57:11 +0200

    feat: add className prop to Main component

--- src/components/layout/Main/Main.jsx
+++ src/components/layout/Main/Main.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import classnames from "classnames";
 
-export default function Main({ children, singleColumn = false }) {
+export default function Main({
+  className = "",
+  children,
+  singleColumn = false,
+}) {
   return (
     <main
-      className={classnames("flex flex-col flex-1 layout", {
-        "layout-px": singleColumn,
-      })}
+      className={classnames(
+        "flex flex-col flex-1 layout",
+        {
+          "layout-px": singleColumn,
+        },
+        className
+      )}
     >
       {children}
     </main>
