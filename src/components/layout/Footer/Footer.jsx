1ba18ea37f9999f0314dea81f6afde83e00dd5c7
ripa <22086553+ripperi@users.noreply.github.com>
2024-01-30 15:22:08 +0200

    Tweak footer column alignment

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -10,18 +10,18 @@ export default function Footer({ title = "", data = [] }) {
             return (
               <React.Fragment key={i}>
                 {i === 0 && (
-                  <div className="type-ui w-full relative leading-none">
+                  <div className="body-sm w-full relative">
                     <span className="absolute flex justify-center items-center h-full w-5 lg:w-10 xl:w-12 -ml-5 lg:-ml-10 xl:-ml-12">
                       ~
                     </span>
                     {section.title}
                   </div>
                 )}
-                <div className="pt-8 w-1/2 md:w-1/3 flex flex-col shrink">
+                <div className="pt-8 xs:mr-5 w-1/2 xs:w-1/3 lg:w-48 xl:w-52 flex flex-col shrink">
                   {section.links.map((link) => {
                     return (
                       <Link
-                        className="type-ui hover:text-brite pb-1"
+                        className="body-sm hover:text-brite"
                         key={link.href}
                         href={link.href}
                         passHref
@@ -35,12 +35,17 @@ export default function Footer({ title = "", data = [] }) {
             );
           })}
         </section>
-        <section className="flex flex-col md:flex-row w-full pt-16 pb-8 ">
-          {data?.[1]?.map((link) => {
+        <section className="flex flex-col xs:flex-row w-full pt-16 pb-8 ">
+          {data?.[1]?.map((link, i) => {
             return (
-              <div key={link.href} className="md:w-1/3">
+              <div
+                key={link.href}
+                className={`w-1/2 xs:w-1/3 lg:w-48 xl:w-52 ${
+                  i > 0 ? "xs:pl-5" : ""
+                }`}
+              >
                 <Link
-                  className="type-ui hover:text-brite"
+                  className="body-sm hover:text-brite"
                   href={link.href}
                   passHref
                 >
