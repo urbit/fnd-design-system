1357d54a02a21062516187c439f14f41d0b00743
ripa <22086553+ripperi@users.noreply.github.com>
2024-02-02 17:28:49 +0200

    Fix modal for screen sizes that exceed maximum width

--- src/components/markdown/Tooltip.js
+++ src/components/markdown/Tooltip.js
@@ -39,54 +39,56 @@ function Tooltip({ data, label }) {
     <>
       {isOpen && (
         <div className="fixed flex items-center justify-center z-50 left-0 top-0 w-screen h-screen bg-[rgba(0,0,0,0.7)]">
-          <div className="flex flex-col bg-gray modal" ref={ref}>
-            <div className="flex w-full justify-between items-center">
-              <div className="text-brite space-x-1">
-                {slug.map((crumb, i) => {
-                  const isFirst = i === 0;
-                  const isLast = i + 1 === slug.length;
-                  return (
-                    <>
-                      {!isFirst && <span>/</span>}
-                      {!isLast && (
-                        <Link
-                          className="md-exclude !no-underline"
-                          href={"/" + join(...slug.slice(0, i + 1))}
-                        >
-                          {capitalize(crumb)}
-                        </Link>
-                      )}
-                      {isLast && (
-                        <Link
-                          className="md-exclude !no-underline"
-                          href={data.slug}
-                        >
-                          {capitalize(crumb.split("#")[0])}
-                        </Link>
-                      )}
-                    </>
-                  );
-                })}
+          <div className="flex items-center justify-center h-full layout">
+            <div className="flex flex-col bg-gray modal" ref={ref}>
+              <div className="flex w-full justify-between items-center">
+                <div className="text-brite space-x-1">
+                  {slug.map((crumb, i) => {
+                    const isFirst = i === 0;
+                    const isLast = i + 1 === slug.length;
+                    return (
+                      <>
+                        {!isFirst && <span>/</span>}
+                        {!isLast && (
+                          <Link
+                            className="md-exclude !no-underline"
+                            href={"/" + join(...slug.slice(0, i + 1))}
+                          >
+                            {capitalize(crumb)}
+                          </Link>
+                        )}
+                        {isLast && (
+                          <Link
+                            className="md-exclude !no-underline"
+                            href={data.slug}
+                          >
+                            {capitalize(crumb.split("#")[0])}
+                          </Link>
+                        )}
+                      </>
+                    );
+                  })}
+                </div>
+                <button className="text-brite" onClick={() => setOpen(false)}>
+                  Close
+                </button>
               </div>
-              <button className="text-brite" onClick={() => setOpen(false)}>
-                Close
-              </button>
+              <hr className="md-exclude hr-horizontal border-black my-4" />
+              <h2
+                className="md-exclude h2 text-black mb-8"
+                dangerouslySetInnerHTML={{ __html: data.title }}
+              />
+              <div
+                className="md-exclude space-y-4"
+                dangerouslySetInnerHTML={{ __html: data.content }}
+              />
+              {/* <Link */}
+              {/*   className="w-min mt-4 btn btn-light md-exclude" */}
+              {/*   href={data.slug} */}
+              {/* > */}
+              {/*   Read More */}
+              {/* </Link> */}
             </div>
-            <hr className="md-exclude hr-horizontal border-black my-4" />
-            <h2
-              className="md-exclude h2 text-black mb-8"
-              dangerouslySetInnerHTML={{ __html: data.title }}
-            />
-            <div
-              className="md-exclude space-y-4"
-              dangerouslySetInnerHTML={{ __html: data.content }}
-            />
-            {/* <Link */}
-            {/*   className="w-min mt-4 btn btn-light md-exclude" */}
-            {/*   href={data.slug} */}
-            {/* > */}
-            {/*   Read More */}
-            {/* </Link> */}
           </div>
         </div>
       )}
