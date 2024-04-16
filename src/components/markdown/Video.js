dc26ecf56d6c5739eb791a8ace487402f2780bfa
ripa <22086553+ripperi@users.noreply.github.com>
2024-04-16 15:09:59 +0300

    Add key to video component
    Fixes issue where video would stick until refresh when user would hop between
    pages with different videos with same placement

--- src/components/markdown/Video.js
+++ src/components/markdown/Video.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Video({ src, type }) {
   return (
-    <div className="-mx-4 md-spacing">
+    <div className="-mx-4 md-spacing" key={src}>
       <video className="mx-auto max-h-[33rem] rounded" controls>
         <source src={src} type={type} />
       </video>
