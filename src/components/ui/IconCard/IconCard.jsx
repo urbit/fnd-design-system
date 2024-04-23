4727b25cc890b492aa235a114dc446aba3776d20
ripa <22086553+ripperi@users.noreply.github.com>
2024-04-23 14:34:03 +0300

    Add hover state to IconCard

--- src/components/ui/IconCard/IconCard.jsx
+++ src/components/ui/IconCard/IconCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import classnames from "classnames";
 import Icon from "../Icon";
@@ -15,22 +15,40 @@ export default function IconCard({
   weight = "regular",
   small = false,
 }) {
+  const [hover, setHover] = useState(false);
   const t = target || defaultTarget(href);
 
   return (
     <>
       {!small && (
-        <Link className="flex rounded-2xl w-full" href={href} target={t}>
+        <Link
+          className="flex rounded-2xl w-full"
+          href={href}
+          target={t}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
           {icon && (
-            <div className="relative flex items-center h-full w-1/5 md:w-1/4 p-2.5 rounded-l-2xl bg-tint">
+            <div
+              className={classnames(
+                "relative flex items-center rounded-l-2xl",
+                "h-full w-1/5 md:w-1/4 p-2.5 bg-tint",
+                /* { "bg-gray": hover, "bg-tint": !hover }, */
+              )}
+            >
               <Icon className="flex-1 bg-brite mx-auto" name={icon} />
             </div>
           )}
           <div
             className={classnames(
               "flex-1 flex flex-col justify-between",
-              "h-full p-2.5 bg-gray md-exclude",
-              { "rounded-r-2xl": icon, "rounded-2xl": !icon }
+              "h-full p-2.5 md-exclude",
+              {
+                "rounded-r-2xl": icon,
+                "rounded-2xl": !icon,
+                "bg-tint": hover,
+                "bg-gray": !hover,
+              },
             )}
           >
             <h3 className="text-brite h3 mb-4 md-exclude">{title}</h3>
@@ -42,9 +60,14 @@ export default function IconCard({
       )}
       {small && (
         <Link
-          className="flex flex-col p-2.5 rounded-2xl bg-gray w-full md-exclude"
+          className={classnames(
+            "flex flex-col p-2.5 rounded-2xl bg-gray w-full md-exclude",
+            { "bg-tint": hover, "bg-gray": !hover },
+          )}
           href={href}
           target={t}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
         >
           <div className="flex items-center h3 h-[1em] mb-3.5 md-exclude">
             <h3 className="text-brite md-exclude">{title}</h3>
